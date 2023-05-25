f0e770b9ddc78006ffb03b71ce910aa8cbd625f9
CarlosOrt2608 <132014917+CarlosOrt2608@users.noreply.github.com>
2023-05-25 14:53:04 -0600

    agregando

---
+++ examenfinal/src/presentacion.java
@@ -0,0 +1,8 @@
+public class presentacion {
+    public static void author071252() {
+        String nombre = "Carlos E Ortega Camara";
+        int matricula = 71252;
+        String correo = "al071252@uacam.mx";
+        System.out.println(nombre + "\n" + matricula + "\n" +correo);
+    }
+}
